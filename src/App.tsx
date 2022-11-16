4e41cd73d7ac7de7a94192e6ccc22ca68ab33052
Joseph Saada <jsaada2003@gmail.com>
2022-11-16 14:34:07 -0800

    added access token to .env file

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,13 @@ import {
   Image,   
   Center,
 } from "@chakra-ui/react"
+ 
+const Access_Token = process.env.REACT_APP_ACCESS_TOKEN
 
-export const App = () => ( 
-  <ChakraProvider theme={theme}>   
+export const App = () => (  
+  <ChakraProvider theme={theme}>    
    <Layout></Layout>   
-   <Center>   
+   <Center>    
    <Image src="doggies.jpg" height="500px" justifyContent="center"/> 
     </Center>
   </ChakraProvider> 
